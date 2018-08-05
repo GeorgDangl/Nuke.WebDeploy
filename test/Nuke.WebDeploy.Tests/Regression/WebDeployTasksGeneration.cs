1e8eb49c59d37f60faed15b7b0bbd905bd570d68
Georg Dangl <georg@dangl.me>
2018-08-05 17:47:58 +0200

    Update code generation to include generation of settings extensions
    This was on by default in earlier NUKE versions, since 0.6.0 it's off by default

---
+++ test/Nuke.WebDeploy.Tests/Regression/WebDeployTasksGeneration.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Nuke.WebDeploy.Tests.Regression
+{
+    public class WebDeployTasksGeneration
+    {
+        [Fact]
+        public void HasGeneratedExtensionMethodsToConfigureProperties()
+        {
+            var setting = new WebDeploySettings().SetPublishUrl("https://example.com");
+            Assert.NotNull(setting);
+            Assert.Equal("https://example.com", setting.PublishUrl);
+        }
+    }
+}
